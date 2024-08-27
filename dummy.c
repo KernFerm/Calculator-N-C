cf934a2b38c204c6030508729a18957b5a26763a
bubbles the dev <152947339+KernFerm@users.noreply.github.com>
2024-08-27 08:44:42 -0400

    Update and rename temp.c to dummy.c
    Signed-off-by: bubbles the dev <152947339+KernFerm@users.noreply.github.com>

---
+++ dummy.c
@@ -0,0 +1,9 @@
+// dummy.c
+// This is a dummy file until the actual code is uploaded.
+
+#include <stdio.h>
+
+int main() {
+    printf("This is a dummy file until acutal code is uploaded. \n");
+    return 0;
+}
